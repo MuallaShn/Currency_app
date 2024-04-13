7ee4bef9feb48e7af80c6a559c64307534d49d16
MuallaShn <muallashn11@gmail.com>
2024-04-13 10:12:04 +0300

    uploading codes

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,40 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/currency.css';
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
-function Currency(){
+import axios from 'axios';
 
-    return(
+
+
+let Base_URL = "https://api.freecurrencyapi.com/v1/latest";
+let API_KEY = "fca_live_bOK63yFKYEqI5NaEgbiaxJ2feDZnTXAhRn09ssc8";
+
+
+
+
+function Currency() {
+
+    const [amount, setAmount] = useState();
+    const [fromCurrency, setFromCurrency] = useState('USD');
+    const [toCurrency, setToCurrency] = useState('TRY');
+    const [result, setResult] = useState();
+
+
+
+    const exchange = async () => {
+        const response = await axios.get(`${Base_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`);
+        const result = (response.data.data[toCurrency] * amount).toFixed(2);
+        setResult(result);
+
+    }
+
+    return (
         <div className="currency-div">
-            <div style={{ fontFamily:'arial', backgroundColor:'black',color:'#fff', width:'100%', textAlign:'center'}}>
+            <div style={{ fontFamily: 'arial', backgroundColor: 'black', color: '#fff', width: '100%', textAlign: 'center' }}>
                 <h3 >DÖVİZ KURU UYGULAMASI</h3>
             </div>
-            <div style={{marginTop:'25px'}}>
-            <input type="number" className="amount" />
-            <select className="from-currency-option">
-                <option >USD</option>
-                <option >EUR</option>
-                <option >TL</option>
-            </select>
+            <div style={{ marginTop: '25px' }}>
+                <input value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                    type="number" className="amount" />
 
-            <FaRegArrowAltCircleRight style={{fontSize:'25px', marginRight: '10px'   }}/>
+                <select onChange={(e) => setFromCurrency(e.target.value)} className="from-currency-option">
+                    <option >USD</option>
+                    <option >EUR</option>
+                    <option >TRY</option>
+                </select>
 
-            <select className="to-currency-option">
-                <option >TL</option>
-                <option >USD</option>
-                <option >EUR</option>
-                
-            </select>
+                <FaRegArrowAltCircleRight style={{ fontSize: '25px', marginRight: '10px'}} />
 
-            <input type="number" className="result" />
+                <select onChange={(e) => setToCurrency(e.target.value)} className="to-currency-option">
+                    <option >TRY</option>
+                    <option >USD</option>
+                    <option >EUR</option>
 
-            
+                </select>
+
+                <input value={result} type="number" className="result" />
+
+
             </div>
 
             <div>
-            <button className='exchange-button'>Çevir</button>
+                <button onClick={exchange}
+                    className='exchange-button'>Çevir</button>
             </div>
-           
-           
+
+
         </div>
     )
 }
